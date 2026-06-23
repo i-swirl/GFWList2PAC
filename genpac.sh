a863e377e682314841f34146fe8afdae419d68b5
i-swirl <iswirl@163.com>
2026-06-23 21:28:18 +0800

    fix bug

--- genpac.sh
+++ genpac.sh
@@ -14,7 +14,7 @@ genpac --format=pac \
 	--gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" \
 	--gfwlist-local="$SH_PATH/gfwlist.txt" \
 	--gfwlist-update-local \
-    --user-rule="||wikipedia.org" \
+	--user-rule="||wikipedia.org" \
 	--user-rule="||google.com" \
 	--user-rule="@@sina.com" \
 	--user-rule="twitter.com" \
@@ -22,13 +22,3 @@ genpac --format=pac \
 	--user-rule-from="$SH_PATH/user-rules.txt" \
 	--gfwlist-decoded-save="$SH_PATH/gfwlist-decoded.file" 
 
-:<<eof
-genpac --format=pac \
-	--pac-proxy="SOCKS5 192.168.1.10:1988" \
-	--proxy="http://192.168.1.10:1988"
-	--gfwlist-local=./gfwlist.txt \
-	--update-gfwlist-local \
-	--user-rule-from=./user.rule \
-	--gfwlist-decoded-save=./gfwlist-decoded.file \
-	--output=./genpac.pac
-eof
